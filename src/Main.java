3f2ebf17c3a692501566d333aa736f74bae36ee8
UfukGngr <ufukgngr94@gmail.com>
2022-11-14 23:38:20 +0300

    www.patika.dev

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int count=0;
+
+       for (int i=2;i<100;i++){
+           int check=0;
+           for (int j=2;j<i;j++){
+               if (i % j ==0 ){
+                   check=1;
+                   break;
+               }
+           }
+           if (check==0){
+               System.out.print(i+" ");
+               count++;
+           }
+        }
+
+
+
+    }
+}
